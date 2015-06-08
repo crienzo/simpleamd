5103f30ac68fada1e8762caa8259584b1d4c9826
Chris Rienzo <chris@rienzo.com>
2015-06-08 12:06:56 -0400

    Tuning tool improvements

--- src/simpleamd.c
+++ src/simpleamd.c
@@ -9,6 +9,15 @@
 #include <errno.h>
 #include <stdlib.h>
 
+#define DEBUG_LOGS 1
+
+#define UNKNOWN "unknown"
+#define HUMAN "human"
+#define MACHINE "machine"
+
+int have_result = 0;
+const char *result = UNKNOWN;
+
 static void amd_logger(samd_log_level_t level, void *user_log_data, const char *file, int line, const char *message)
 {
 	printf("%s:%d\t%s", file, line, message);
@@ -16,7 +25,12 @@ static void amd_logger(samd_log_level_t level, void *user_log_data, const char *
 
 static void amd_event_handler(samd_event_t event, uint32_t time_ms, void *user_event_data)
 {
-	//printf("%d %s\n", time_ms, samd_event_to_string(event));
+	if (event == SAMD_MACHINE_VOICE || event == SAMD_MACHINE_SILENCE) {
+		result = MACHINE;
+	} else if (event == SAMD_HUMAN_SILENCE || event == SAMD_HUMAN_VOICE) {
+		result = HUMAN;
+	}
+	have_result = 1;
 }
 
 int main(int argc, char **argv)
@@ -37,7 +51,10 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Failed to initialize AMD VAD\n");
 		exit(EXIT_FAILURE);
 	}
+	samd_vad_set_silence_ms(vad, 500); /* wait 1/2 second for end of voice */
+#if DEBUG_LOGS
 	samd_vad_set_log_handler(vad, amd_logger, NULL);
+#endif
 
 	samd_init(&amd, vad);
 	if (!amd) {
@@ -45,7 +62,9 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	samd_set_event_handler(amd, amd_event_handler, NULL);
+#if DEBUG_LOGS
 	samd_set_log_handler(amd, amd_logger, NULL);
+#endif
 
 	raw_audio_file = fopen(argv[1], "rb");
 	if (!raw_audio_file) {
@@ -53,17 +72,18 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	while (!feof(raw_audio_file) && !ferror(raw_audio_file)) {
-		size_t num_samples = fread(samples, sizeof(int16_t), 4098, raw_audio_file);
+	while (!feof(raw_audio_file) && !ferror(raw_audio_file) && !have_result) {
+		size_t num_samples = fread(samples, sizeof(int16_t), 80, raw_audio_file);
 		if (num_samples > 0) {
 			samd_process_buffer(amd, samples, num_samples);
 		}
 	}
 
 	fclose(raw_audio_file);
-	samd_vad_destroy(&vad);
 	samd_destroy(&amd);
+	samd_vad_destroy(&vad);
+
+	printf("%s,%s\n", argv[1], result);
 
 	return EXIT_SUCCESS;
 }
-
